761003bab490545979e0772758c269e1b94119a0
palashkumardas <info.palash27@gmail.com>
2023-08-04 14:06:31 +0600

    show name into select

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup>
+import { ref, reactive } from 'vue'
+const persons = reactive([
+  { name: 'John Doe', age: 25, email: 'john@doe.com' },
+  { name: 'Jane Doe', age: 26, email: 'jane@doe.com' },
+  { name: 'John Smith', age: 27, email: 'john@smith.com' },
+  { name: 'Jane Smith', age: 28, email: 'jane@smith.com' }
+])
 
+const activeIndex = ref(0)
 </script>
 
 <template>
-  
+    <section class="mx-auto container text-left">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="my-5">{{ persons }}</p>
+    <div class="flex ">
+      <div class="w-1/2">
+        <p>{{ activeIndex }}</p>
+        
+        <div class="mb-10">
+        <h2>Select a person</h2>
+        <select name="person" id="person" v-model="activeIndex" >
+          <option :value="index" v-for="(person,index) in persons" :key="index" >{{person.name}}</option>
+        </select>
+      </div>
+      </div>
+      <div>
+        <div class="flex flex-col">
+          <input class=" my-3" type="text"  placeholder="name" >
+        </div>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <style scoped>
